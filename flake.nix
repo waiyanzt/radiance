7b2dd944aa1831d854abbc0d54ded84b78c84834
Marcus Tin <marcus.tinw@gmail.com>
2025-10-23 18:23:12 -0700

    nixnixnix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Ray Tracer in One Weekend";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            rust-analyzer
+            rustfmt
+            clippy
+          ];
+
+          shellHook = ''
+            echo "🦀 Ray Tracer Environment"
+            echo "Rust: $(rustc --version)"
+            echo "Cargo: $(cargo --version)"
+          '';
+        };
+      }
+    );
+}
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // Image
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+    println!("P3");
+    println!("{} {]}", image_width, image_height);
+    println!("255");
+
+    for j in (0...image_height).rev() {
+        eprintln!("Scanlines remaining: {}", j);
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.25;
+        }
+    }
+}
